6a20920587425faaeeda109d0adfdbf82f204227
GodLis <olechka.brajko@gmail.com>
2017-01-29 13:58:25 +0200

    start development

---
+++ header.php
@@ -0,0 +1,60 @@
+<?php
+/*include_once("includes/connection.php");
+session_start();*/
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>It`s my diploma</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="container-fluid">
+
+    <div class="navbar navbar-default navbar-fixed-top navbar-inverse">
+        <div class="navbar-inner">
+            <div class="container">
+
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".nav-collapse" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">Інтерактивний Web-ресурс з курсу "Web-програмування. Напрям PHP"</a>
+                    <!--<a class="navbar-brand"><img src="images/logo1.png"></a>-->
+                </div>
+
+                <div class="navbar-collapse collapse nav-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">JUMBOTRON</a></li>
+
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">PHP. Основи
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">PHP. The Right Way</a></li>
+                                <li><a href="">Стандарти PHP</a></li>
+                                <li><a href="">Тестування</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="">PHP. The Right Way</a></li>
+                        <li><a href="">Стандарти PHP</a></li>
+                        <li><a href="">Тестування</a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("header.php");
+?>
+    <div>jdfsbh </div>
+
+<?php include_once("footer.php");
