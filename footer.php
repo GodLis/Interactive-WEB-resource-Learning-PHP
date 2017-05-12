d49dcd54e0124bb599f0d287489bd8350965bed8
GodLis <olechka.brajko@gmail.com>
2017-05-13 00:44:14 +0300

    update phpTheRightWay

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <div class="panel-footer coll-md-12 text-info center-block text-center" id="footer">
-    Dev by <a href="https://vk.com/olechkabrajko">Olechka Brajko</a> 2017
+    Dev by <a href="https://github.com/GodLis">Olechka Brajko</a> 2017
 </div>
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
