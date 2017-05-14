17ac3e39b38932691bdcb859c7f0a6f5fe02e8d4
GodLis <olechka.brajko@gmail.com>
2017-05-14 15:25:50 +0300

    update visual adaptation and session data

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-/*include_once("includes/connection.php");
-session_start();*/
+include_once("includes/connection.php");
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-    mysqli_select_db($connection, DB_NAME);
+    //$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    //mysqli_select_db($connection, DB_NAME);
